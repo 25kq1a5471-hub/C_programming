5a7224c804ef5bb6658401a8198fedf2f71082f6
25kq1a5471-hub <25kq1a5471@pace.ac.in>
2025-11-27 22:54:28 +0530

    Add files via upload

---
+++ even or odd function .c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+int a;
+printf("enter a:");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("the given number is even");
+}
+printf("the given number is odd");
+return 0;
+}
