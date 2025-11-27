53e085301bbcaded2de0418a2347eec14703ec42
25kq1a5471-hub <25kq1a5471@pace.ac.in>
2025-11-27 23:05:55 +0530

    Add files via upload

---
+++ print 1to5 numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+auto int mainvar;
+void printnum();
+mainvar=10;
+printnum();
+printf("int main:%d",mainvar);
+}
+void printnum()
+{
+auto int mainvar;
+printf("%d",mainvar);
+}
